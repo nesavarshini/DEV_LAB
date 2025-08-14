000e7c04f2fd56eada82d804bfa00258f8b36f56
nesavarshini <nesavarshinimm@gmail.com>
2025-08-14 15:56:47 +0530

    Add files via upload

---
+++ dev1.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+X=[1,2,3,4,5]
+Y=[10,20,30,40,50]
+plt.plot(X,Y)
+plt.show()
+
+import matplotlib.pyplot as plt
+fruits=["Apple","Banana","Cherry","Watermelon","Mango"]
+sales=[400,350,300,450,500]
+plt.bar(fruits,sales)
+plt.title("fruit sales")
+plt.xlabel("fruits")
+plt.ylabel("sales")
+plt.show() 
